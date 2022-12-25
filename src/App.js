5f123c2bc191ab1e4349b3bef1d0bb2578880d31
IgnacioSanhuezaZ <53100726+TheGreySam@users.noreply.github.com>
2022-12-25 20:14:37 -0300

    creacion Card.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <h1>Galería de Imágenes con
+        React</h1>
+    )
+}
+
+export default Card;
