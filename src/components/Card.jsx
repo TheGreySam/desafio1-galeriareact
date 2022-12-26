7ac2d54bf249943fea3b6e8b3c44f5163189c1d9
IgnacioSanhuezaZ <53100726+TheGreySam@users.noreply.github.com>
2022-12-25 21:18:11 -0300

    Incorporacion de Bootstrap

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Card = () => {
     return (
-        <h1>Galería de Imágenes con
-        React</h1>
+        <div className="card" >
+            <img className="card-img-top" src="..." alt="imagen"></img>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    
+                </div>
+        </div>
     )
 }
 
